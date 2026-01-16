b2973daa6e1d9f3d49ff3fd3b88b6f6d31941772
Sameer Chaudhary <154604600+AdmGenSameer@users.noreply.github.com>
2026-01-16 22:09:29 +0530

    Fix closing tag for main element in GenerateId.jsx (#54)

--- Frontend/src/pages/GenerateId.jsx
+++ Frontend/src/pages/GenerateId.jsx
@@ -405,7 +405,7 @@ const GenerateId = () => {
               </div>
             </div>
           </div>
-        </main>flex flex-col max-h-[calc(100vh-2
+        </main>
       </div>
 
       {/* Preview Modal */}
